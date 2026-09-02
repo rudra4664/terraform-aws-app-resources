d0e76d95a5548786b1d7136295c7f2c750d9e41a
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:13:15 +0530

    Define output for EC2 instance resources
    Add output for resources created by EC2 instance module.

--- modules/terraform-aws-ec2-instance/outputs.tf
+++ modules/terraform-aws-ec2-instance/outputs.tf
@@ -1,1 +1,9 @@
-
+output "resources" {
+  description = "Map of all resources created by this module."
+  value = {
+    additional_volume_attachments = module.ec2.resources.additional_volume_attachments
+    additional_volumes            = module.ec2.resources.additional_volumes
+    instance                      = module.ec2.resources.instance
+    primary_network_interface     = module.ec2.resources.primary_network_interface
+  }
+}
