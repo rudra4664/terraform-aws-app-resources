ed9c57959a7c63501a8000ef1df0c3836b6d75a7
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:19:00 +0530

    Configure AWS provider with tags and version
    Added AWS provider configuration with default and ignore tags.

--- examples/terraform-aws-ec2-instance/providers.tf
+++ examples/terraform-aws-ec2-instance/providers.tf
@@ -1,1 +1,33 @@
+provider "aws" {
+  region = var.region
 
+  default_tags {
+    tags = {
+      app_id = "APP-12345"
+    }
+  }
+
+  ignore_tags {
+    keys = [
+      "domain_join",
+      "fqdn",
+      "nyl:appid",
+    ]
+
+    key_prefixes = [
+      "nyl:platform:",
+    ]
+  }
+}
+
+
+terraform {
+  required_version = ">= 1.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 6.45"
+    }
+  }
+}
