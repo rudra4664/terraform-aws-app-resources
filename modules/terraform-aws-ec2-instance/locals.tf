638cda496defd853071fa4e014abe8acb99dba95
rudra4664 <167764527+rudra4664@users.noreply.github.com>
2026-09-02 23:12:15 +0530

    Add locals for instance families and tags

--- modules/terraform-aws-ec2-instance/locals.tf
+++ modules/terraform-aws-ec2-instance/locals.tf
@@ -1,1 +1,8 @@
-
+locals {
+  approved_instance_families = ["t3", "m7i", "c5", "c5a", "r5", "m5a", "m5", "m4", "t2", "r6i", "r7iz", "r6a"]
+  denied_instance_sizes      = ["micro", "nano", "small"]
+  effective_tags             = merge(var.tags, local.mandatory_tags)
+  mandatory_tags = {
+    appid = var.app_id
+  }
+}
